8d6b3ae6917915dc111d51d28740147ec33c61f3
Eusebiu Morar <morar25032002@yahoo.ro>
2026-02-07 19:04:24 +0200

    final commit 12

--- src/main/java/com/example/demo/FileHelper.java
+++ src/main/java/com/example/demo/FileHelper.java
@@ -5,6 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -62,6 +63,41 @@ public class FileHelper {
             for (MultipartFile file : files) {
                 Path destination = Path.of(backupDirectoryWithDate.toString(), file.getOriginalFilename());
                 file.transferTo(destination);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void checkDirectory(Path path) {
+        File file = new File(String.valueOf(path));
+        if (!file.exists() || !file.isDirectory() || !file.canRead() || !file.canWrite()) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void copyFolder(Path sourceFolder, Path targetFolder){
+        try {
+            createDirectory(targetFolder);
+            try (DirectoryStream<Path> files = Files.newDirectoryStream(sourceFolder)) {
+                for (Path file : files) {
+                    if (Files.isRegularFile(file)) {
+                        Path targetFile = targetFolder.resolve(file.getFileName());
+                        Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAllFiles(Path folder) {
+        try (DirectoryStream<Path> files = Files.newDirectoryStream(folder)) {
+            for (Path file : files) {
+                if (Files.isRegularFile(file)) {
+                    Files.delete(file);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
---
+++ src/main/java/com/example/demo/fileUploader/FileUploaderService.java
@@ -0,0 +1,76 @@
+package com.example.demo.fileUploader;
+
+import com.example.demo.FileHelper;
+import com.example.demo.files.FileServiceOrchestrator;
+import com.example.demo.model.FileMetadata;
+import com.example.demo.model.FileMetadataFactory;
+import com.example.demo.service.FileMetaDataService;
+import com.example.demo.service.UserService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.example.demo.service.UserService.userDTO;
+@Service
+public class FileUploaderService {
+
+
+    private final FileServiceOrchestrator fileServiceOrchestrator;
+    private final UserService userService;
+    private final FileMetaDataService fileMetaDataService;
+    private final FileHelper fileHelper;
+    private final FileMetadataFactory fileMetadataFactory;
+    @Value("${file.uploader.job.date.format}")
+    private String DATE_FORMAT;
+    @Value("#{T(java.nio.file.Paths).get('${file.uploader.job.backup.path}')}")
+    private Path backupPath;
+    @Value("#{T(java.nio.file.Paths).get('${file.uploader.job.failed.path}')}")
+    private Path failedPath;
+    private DateTimeFormatter formatter;
+
+    @Autowired
+    public FileUploaderService(
+            FileServiceOrchestrator fileServiceOrchestrator,
+            UserService userService,
+            FileMetaDataService fileMetaDataService,
+            FileHelper fileHelper,
+            FileMetadataFactory fileMetadataFactory
+    ) {
+        this.fileServiceOrchestrator = fileServiceOrchestrator;
+        this.userService = userService;
+        this.fileMetaDataService = fileMetaDataService;
+        this.fileHelper = fileHelper;
+        this.fileMetadataFactory = fileMetadataFactory;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        fileHelper.checkDirectory(backupPath);
+        fileHelper.checkDirectory(failedPath);
+    }
+
+    @Retryable(retryFor = Exception.class)
+    public void process(File file) {
+        FileMetadata fileMetadata = fileMetadataFactory.create(file, userService.getOwnerId(userDTO));
+        if (!fileMetaDataService.checkFileExists(fileMetadata)) {
+            fileServiceOrchestrator.uploadFile(file, fileMetadata);
+        }
+        fileHelper.move(file, Path.of(backupPath.toString(), LocalDate.now().format(formatter)));
+    }
+
+    @Recover
+    public void recover(Exception e, File file) throws IOException {
+        fileHelper.move(file, Path.of(failedPath.toString(), LocalDate.now().format(formatter)));
+    }
+}
