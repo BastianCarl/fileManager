d43c29969121a14782555d2c13140cf6d9921188
Eusebiu Morar <morar25032002@yahoo.ro>
2026-03-05 19:26:14 +0200

    final commit 57

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,12 +6,15 @@ import com.example.demo.service.UserService;
 import com.example.demo.utility.JWTHelper;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/auth")
 @RestController
 @AllArgsConstructor
 public class UserController {
@@ -20,18 +23,18 @@ public class UserController {
     private final AuthenticationManager authenticationManager;
     private final JWTHelper jwtHelper;
 
-    @PostMapping("registration")
+    @PostMapping("/registration")
     public User register(@RequestBody UserDTO user) {
        return userService.saveUser(new User(user));
     }
 
-    @PostMapping("login")
+    @PostMapping("/login")
     public String login(@RequestBody UserDTO user) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
         if (authentication.isAuthenticated()) {
             return jwtHelper.generateToken(user.getUserName());
         } else {
-            return "fail";
+            throw new BadCredentialsException("Invalid credentials");
         }
     }
 }
