a1113d795dd21e8bd936dac9c072aa2af2b1e9e7
Eusebiu Morar <morar25032002@yahoo.ro>
2026-03-15 12:02:18 +0200

    final commit 58

--- src/main/java/com/example/demo/repository/AnimalRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimalRepository extends JpaRepository<Animal, Long> { }
